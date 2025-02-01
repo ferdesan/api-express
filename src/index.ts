6758eebb7dd7b0f15c12fc50798905ec045fa205
Raniel Santana <ferdesan@.gmail.com>
2025-01-31 22:17:03 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response} from "express";
+
+// cria a aplicação express
+const app = express();
+
+// criar uma rota GET do app principal
+app.get("/", (req: Request, res:Response) => {
+    res.send("Seja bem vindo!");
+})
+
+//iniciar o servidor na porta 8090
+app.listen(8090, () => {
+    console.log(" O servidor foi iniciado na porta 8090 com sucesso: http://localhost:8090")
+})
+
