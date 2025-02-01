6fae9c9eceb862e58f8493d40fa3377a09576816
Raniel Santana <ferdesan@.gmail.com>
2025-02-01 00:02:51 -0300

    ajuste porta para 8090 atual

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,6 @@ app.get("/", (req: Request, res:Response) => {
 
 //iniciar o servidor na porta 8090
 app.listen(8090, () => {
-    console.log(" O servidor foi iniciado na porta 8090 com sucesso: http://localhost:8090");
+    console.log(" O servidor foi iniciado na porta 8090 com sucesso!: http://localhost:8090");
 })
 
