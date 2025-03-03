32d02d8ffebb05066e1ccf0301b40d3b33a15dd3
Raniel Santana <ferdesan@gmail.com>
2025-03-03 10:38:25 -0300

    feat: add recurso para encerrar servidor

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,29 @@
-import {app} from "./app";
+import { app } from "./app";
 
 import { routerUsers } from "./controllers/UsersControllers";
 
-//acessar as rotas de UsersControllers
+/**
+ * acessar as rotas de UsersControllers
+ * @param {string} path - caminho da rota
+ * @param {Router} routerUsers - rota de UsersControllers
+ */
 app.use("/", routerUsers);
 
-//iniciar o servidor na porta 8090
+/**
+ * iniciar o servidor na porta 8090
+ * @param {number} port - porta do servidor
+ */
 const port = process.env.PORT || 8090;
 app.listen(port, () => {
   console.log(
     " O servidor foi iniciado na porta 8090 com sucesso!: http://localhost:8090"
   );
+
+  /**
+   * encerrar o servidor
+   */
+  process.on("SIGINT", () => {
+    console.log("Servidor encerrado");
+    process.exit();
+  });
 });
